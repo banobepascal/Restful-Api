92a3e5f029de2ecbae0b60f16a49a10cf8ac88e0
banobepascal <banobepascal@gmail.com>
2019-08-11 10:53:24 +0300

    adding courses object

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const courses = [
+    {id: 1, name: course1},
+    {id: 2, name: course2},
+    {id: 3, name: course3}
+];
+
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: "Yes you have handled a get request"
     });
+});
+
+router.get('/courses/:id', (req, res, next) => {
+    const courese = courses.find(c => c.id === parseInt(req.params.id));
 });
 
 router.post('/', (req, res, next) => {
